5db09c7cb0194ba7e271055b4cc409260e61c512
SeungAhSon <gongsoonyee@gmail.com>
2024-03-25 15:07:14 +0900

    make Experiement3 folder

---
+++ Experiment3/build/gtest/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /usr/src/googletest
+# Build directory: /home/sa/RE510_2024/Experiment3/build/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
