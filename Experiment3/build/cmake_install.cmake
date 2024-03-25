b6ea51ee54cf8581dcc6d43b7c4a366d5fdbab73
SeungAhSon <gongsoonyee@gmail.com>
2024-03-25 20:11:57 +0900

    Class 1

--- Experiment3/build/cmake_install.cmake
+++ Experiment3/build/cmake_install.cmake
@@ -131,6 +131,7 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
   include("/home/sa/RE510_2024/Experiment3/build/gtest/cmake_install.cmake")
+  include("/home/sa/RE510_2024/Experiment3/build/tutorial/cmake_install.cmake")
 
 endif()
 
